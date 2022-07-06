58f22a0e8220f3a2c2f4a42de01b7030309dde88
zubin-madon <madon.zubin@gmail.com>
2022-07-06 17:16:41 +0530

    metatags for social media

--- pages/index.js
+++ pages/index.js
@@ -16,13 +16,38 @@ export default function Home() {
   
     return (
       <Fragment>
+      <Head>
+        <title>Cryft NFTs</title>
+        <meta
+          name="description"
+          content="Cryft NFT Dapp"
+        />
+        <meta charset="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Cryft NFT Dapp" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta property="og:title" content="Cryft NFTs" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://cryft-nft.netlify.app" />
+        <meta
+          property="og:image"
+          content="/android-chrome-512x512.png"
+        />
+        <meta property="og:description" content="Cryft NFT Dapp" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+
+
+
         {!isAuthenticated ? (
           <div className="grid place-items-center h-screen">
             <Login />
           </div>
         ) : (
           <div className="text-2xl">
-            <h1 className="text-white"> Home Authentication complete </h1>
+            <h1 className="text-white"> Authentication complete! </h1>
       
           </div>
         )}
